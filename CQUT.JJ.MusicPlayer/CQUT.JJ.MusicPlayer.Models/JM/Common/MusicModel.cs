354dc8a8d123108738041ebaeabba6ed827e8068
xiaoxiao <jiajianj2057@qq.com>
2018-03-28 16:38:37 +0800

    修改部分库为标准库

---
+++ CQUT.JJ.MusicPlayer/CQUT.JJ.MusicPlayer.Models/JM/Common/MusicModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQUT.JJ.MusicPlayer.Models.JM.Common
+{
+    [Serializable]
+    [DataContract]
+    public class MusicModel
+    {
+        /// <summary>
+        /// 音乐id
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 歌唱家id
+        /// </summary>
+        [DataMember]
+        public int SingerId { get; set; }
+
+        /// <summary>
+        /// 专辑id
+        /// </summary>
+        [DataMember]
+        public int AlbumId { get; set; }
+
+        /// <summary>
+        /// 音乐名
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 歌唱家名字
+        /// </summary>
+        [DataMember]
+        public string SingerName { get; set; }
+
+        /// <summary>
+        /// 专辑名
+        /// </summary>
+        [DataMember]
+        public string AlbumName { get; set; }
+
+        /// <summary>
+        /// 时长
+        /// </summary>
+        [DataMember]
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        [DataMember]
+        public string FileUrl { get; set; }
+    }
+}
