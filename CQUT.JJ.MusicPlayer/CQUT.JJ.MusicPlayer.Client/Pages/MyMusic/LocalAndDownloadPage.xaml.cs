f465745a5bc2542e9a6f2be7667b7343213ed79b
xiaoxiao <jiajianj2057@qq.com>
2018-04-09 17:22:35 +0800

    完成本地与下载

--- CQUT.JJ.MusicPlayer/CQUT.JJ.MusicPlayer.Client/Pages/MyMusic/LocalAndDownloadPage.xaml.cs
+++ CQUT.JJ.MusicPlayer/CQUT.JJ.MusicPlayer.Client/Pages/MyMusic/LocalAndDownloadPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CQUT.JJ.MusicPlayer.Client.Utils.Configs;
+using CQUT.JJ.MusicPlayer.Client.ViewModels;
+using CQUT.JJ.MusicPlayer.EntityFramework.Persistences;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,25 @@ namespace CQUT.JJ.MusicPlayer.Client.Pages.MyMusic
     /// </summary>
     public partial class LocalAndDownloadPage : Page
     {
+        private static List<MusicInfoViewModel> _musicListViewModel = null;
+
         public LocalAndDownloadPage()
         {
             InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var downloadPath = await SettingUtil.GetDowloadPathAsync();
+            _musicListViewModel = Directory.GetFiles(downloadPath)
+                .Where(m => GlobalConstants.Music_File_Suffix.Contains(System.IO.Path.GetExtension(m)))
+                .Select(m => new MusicInfoViewModel()
+                {
+                    MusicName = System.IO.Path.GetFileNameWithoutExtension(m),
+                    FileUrl = m
+                })
+                .ToList();
+            MusicList.ItemsSource = _musicListViewModel;
         }
     }
 }
